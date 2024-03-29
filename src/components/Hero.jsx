61c48c16f794d2fb2364911856a8e6ca244a3578
Rahul Kumar <rahul@Rahuls-MacBook-Pro.local>
2024-03-29 14:11:38 +0530

    minor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,14 @@ const Hero = () => {
   return (
     <div className="flex flex-col sm:flex-row justify-between items-center h-[250px] bg-[#F1D732]">
       <div className=" h-full w-full flex justify-center items-center mt-8">
-        <div className="h-[150px] w-[250px] bg-[#1B1D36] rounded-xl flex gap-3 items-center flex-col shadow-xl">
-          <center className="text-[28px] leading-7 text-[#F1D732] font-normal mt-3">
+        <div className="h-[150px] w-full mx-20 min-w-[200px] px-10 md:px-20 bg-[#1B1D36] rounded-xl flex gap-5 items-center justify-center flex-col shadow-xl">
+          <center className="text-[28px] leading-7 text-[#F1D732] font-normal">
             Find your destination
           </center>
-          <div className="relative">
+          <div className="relative w-full">
             <input
               type="text"
-              className="rounded-2xl p-1 px-2 "
+              className="rounded-2xl p-1 px-2 w-full"
               placeholder="Search"
             />
             <div className="bg-[#F1D732] p-2 rounded-2xl absolute top-0 right-0 px-4">
